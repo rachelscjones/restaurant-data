9346855bac6607be4c42b394459fdd8a8f7af3b3
Rachel Jones <rachelscjones@gmail.com>
2024-03-01 16:33:28 +0000

    iterate through restaurant hash to output required data in restaurant.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,6 @@ class PagesController < ApplicationController
   def home
   end
 
-# TODO
-# Make a call on the API
-# Optional: first, check if the postcode is valid (https://github.com/threedaymonk/uk_postcode)
-
   require "json"
   require "open-uri"
   require "active_support/hash_with_indifferent_access"
@@ -16,3 +12,7 @@ class PagesController < ApplicationController
     @restaurants = JSON.parse(URI.open("#{url}").read).dig('restaurants')
   end
 end
+
+
+# TODO
+# Optional: first, check if the postcode is valid (https://github.com/threedaymonk/uk_postcode)
