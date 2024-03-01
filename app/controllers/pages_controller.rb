6f8734843995db9645f8e3f843cdc8d1566e5f86
Rachel Jones <rachelscjones@gmail.com>
2024-03-01 16:00:42 +0000

    make call to api, parse json data, access restaurant hash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
 class PagesController < ApplicationController
   def home
   end
+
+# TODO
+# Make a call on the API
+# Optional: first, check if the postcode is valid (https://github.com/threedaymonk/uk_postcode)
+
+  require "json"
+  require "open-uri"
+  require "active_support/hash_with_indifferent_access"
+
+  def restaurant
+    @postcode = params[:postcode]
+    url = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/#{@postcode}"
+    @restaurants = JSON.parse(URI.open("#{url}").read).dig('restaurants')
+  end
 end
